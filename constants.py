502df7ff709a8abfcc8a6551fb936fba4d302c4f
Anustup Mukherjee <60361231+Anustup900@users.noreply.github.com>
2024-11-14 18:09:04 +0530

    Update constants.py

--- constants.py
+++ constants.py
@@ -37,7 +37,8 @@ MODEL_VERSION_DETAILS = {
             "v_parameterization": False,
             "alpha_mask": False,
             "lr_decay_steps": 0,
-            "lr_scheduler_timescale": 0
+            "lr_scheduler_timescale": 0,
+            "lr_scheduler_min_lr_ratio": 0
 
         }
     },
