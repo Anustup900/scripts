4cb299a4533c39da8247df41e1753d7d80e875c1
Anustup Mukherjee <60361231+Anustup900@users.noreply.github.com>
2024-11-14 17:17:11 +0530

    Update constants.py

--- constants.py
+++ constants.py
@@ -36,7 +36,8 @@ MODEL_VERSION_DETAILS = {
             "fused_backward_pass": False,
             "v_parameterization": False,
             "alpha_mask": False,
-            "lr_decay_steps": 0.0001
+            "lr_decay_steps": 0.0001,
+            "lr_scheduler_timescale": None
         }
     },
     MODEL_VERSION_FLUX_KOHYA: {
